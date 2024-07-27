d64d712b92f7ae8ba1fbbcf8a5d8ae84d36b2c05
Davronbek <atadjanovdavronbek863@gmail.com>
2024-07-27 11:01:05 +0500

    Initial

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+# PostgreSQL ma'lumotlar bazasiga bog'lanish uchun engine yaratamiz
+engine = create_engine('postgresql://postgres:12345@localhost/personal_debts_db', echo=True)
+
+# Declarative_base yaratamiz
+Base = declarative_base()
+session = sessionmaker()
+session = session(bind=engine)
